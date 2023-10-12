d7c75c17b64b660bfed2ba3ee7e6d2dce98e3590
ethanmoody27 <ethanmoody27@gmail.com>
2023-10-12 16:43:06 -0400

    All last tab problems

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
-export default function SelectedContact() {
-  return (
+export default function SelectedContact({ selectedContactId, setSelectedContactId }) {
+    const [contact, setContact] = useState(null);
+    
+    useEffect(() => {
+        async function fetchSelectedContact() {
+            try {
+                const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`);
+                const data = await response.json();
+                setContact(data);
+                console.log("Selected contact data :", data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        fetchSelectedContact();
+    }, [selectedContactId]);
+    
+    
+    return (
     <div>
-      
+      <p>Info</p>
     </div>
   )
 }
