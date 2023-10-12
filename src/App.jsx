ddfcdd21f03395df4fc82aa8ba94b80b7dca79d9
ethanmoody27 <ethanmoody27@gmail.com>
2023-10-12 15:44:16 -0400

    Third tab commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import ContactList from "./components/ContactList"
+import SelectedContact from "./components/SelectedContact"
 import './App.css'
 
 function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList setSelectedContactId={selectedContactId} />
+      )}
     </>
   );
 }
--- src/components/ContactRow.jsx
+++ src/components/ContactRow.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-function ContactRow({ contact }) {
+function ContactRow({ setSelectedContactId, contact }) {
     return (
-        <tr>
-            <td>{contact.name}</td>
-            <td>{contact.email}</td>
-            <td>{contact.phone}</td>
+        <tr
+            onClick={() => {
+                setSelectedContactId(contact.id);
+            }}
+            >
+                <td>{contact.name}</td>
+                <td>{contact.email}</td>
+                <td>{contact.phone}</td>
         </tr>
     );
 }
